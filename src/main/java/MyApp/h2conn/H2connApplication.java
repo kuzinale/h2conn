a2699823f38f9ac65c75150f7e8717d6835f0175
kuzin.al <kuzi4kin@gmail.com>
2018-07-24 18:30:10 +0300

    Initial commit

---
+++ src/main/java/MyApp/h2conn/H2connApplication.java
@@ -0,0 +1,12 @@
+package MyApp.h2conn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class H2connApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(H2connApplication.class, args);
+	}
+}
---
+++ src/test/java/MyApp/h2conn/H2connApplicationTests.java
@@ -0,0 +1,16 @@
+package MyApp.h2conn;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class H2connApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
